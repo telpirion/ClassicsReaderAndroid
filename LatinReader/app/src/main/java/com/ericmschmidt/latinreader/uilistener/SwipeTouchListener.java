384cf38189c9661c83e869dbfe4a77b5e05ac159
Eric Schmidt <em.schmidt78@gmail.com>
2018-08-02 19:45:43 -0700

    Updates Latin Reader Android gradle files

---
+++ LatinReader/app/src/main/java/com/ericmschmidt/latinreader/uilistener/SwipeTouchListener.java
@@ -0,0 +1,15 @@
+package com.ericmschmidt.latinreader.uilistener;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by ericmschmidt on 3/18/17.
+ */
+
+public class SwipeTouchListener implements View.OnTouchListener {
+
+    public boolean onTouch(View v, MotionEvent event) {
+        return true;
+    }
+}
