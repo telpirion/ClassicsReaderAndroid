24d89f9d936096b1a2aec2a6b5295e8e362ff3a1
Eric Schmidt <em.schmidt78@gmail.com>
2025-11-13 20:39:50 -0800

    feat: translates Book.java to Kotlin

--- LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/datamodel/Book.kt
+++ LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/datamodel/Book.kt
@@ -1,41 +1,22 @@
-package com.ericmschmidt.classicsreader.datamodel;
+package com.ericmschmidt.classicsreader.datamodel
 
-import java.util.ArrayList;
-import java.util.List;
-
-/** Represents a sub-division within a larger text.
+/**
+ * Represents a sub-division within a larger text.
  * @author Eric Schmidt
- * @author http://telpirion.com
+ * @author <a href="https://telpirion.com">...</a>
  * @version 2019-11-17
  * @since 1.0
  */
-public class Book {
+class Book(val id: Int) {
 
-    private ArrayList<String> _lines;
-    private int _id;
-
-    /**
-     * Creates an instance of the Book class
-     */
-    public Book(int id) {
-        this._lines = new ArrayList<String>();
-        this._id = id;
-    }
+    private val _lines = mutableListOf<String>()
 
     /**
      * Adds a line to the book.
      * @param line the line to add.
      */
-    public void addLines(String line) {
-        this._lines.add(line);
-    }
-
-    /**
-     * Gets the ID of this book within the work.
-     * @return an int specifying this book's position.
-     */
-    public int getId() {
-        return this._id;
+    fun addLines(line: String) {
+        _lines.add(line)
     }
 
     /**
@@ -43,18 +24,14 @@ public class Book {
      * @param position the position of the line from the beginning of the book.
      * @return the line.
      */
-    public String getLine(int position) {
-
-        String line = null;
-        if (position >= 0
-                && position < this._lines.size()) {
-            line = this._lines.get(position);
-        } else if (position > this._lines.size()) {
-            return null;
+    fun getLine(position: Int): String? {
+        return if (position >= 0 && position < _lines.size) {
+            _lines[position]
+        } else if (position > _lines.size) {
+            null
         } else {
-            line = this._lines.get(0); // Default is to return the first line of the book.
+            _lines.firstOrNull() // Default is to return the first line of the book.
         }
-        return line;
     }
 
     /**
@@ -63,31 +40,30 @@ public class Book {
      * @param offset the number of lines to get.
      * @return String the text from the position indicated.
      */
-    public String getLines(int position, int offset) {
-        String text = "";
-        List<String> lines;
+    fun getLines(position: Int, offset: Int): String {
+        if (position < 0) {
+            return getLines(0, offset)
+        }
 
-        if (position < 0)
-            return getLines(0, offset);
+        val toIndex = (position + offset).coerceAtMost(_lines.size)
+        
+        if (position > toIndex) {
+            return ""
+        }
 
-        if (position + offset < this._lines.size()) {
-            lines = this._lines.subList(position, position + offset);
+        val subList = _lines.subList(position, toIndex)
+        return if (subList.isEmpty()) {
+            ""
         } else {
-            lines = this._lines.subList(position, this._lines.size());
+            subList.joinToString("\n", postfix = "\n")
         }
-
-        for (String l : lines) {
-            text += l + "\n";
-        }
-
-        return text;
     }
 
     /**
      * Gets the number of lines in this book.
      * @return an int of the number of lines.
      */
-    public int getLineCount() {
-        return this._lines.size();
+    fun getLineCount(): Int {
+        return _lines.size
     }
 }
