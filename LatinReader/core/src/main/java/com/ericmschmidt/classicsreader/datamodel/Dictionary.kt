712055613f068f3f1588cf1831e2793ac5b9dc9b
Eric Schmidt <erschmid@google.com>
2025-11-14 14:40:27 -0800

    feat: converts Dictionary class to Kotlin

--- LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/datamodel/Dictionary.kt
+++ LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/datamodel/Dictionary.kt
@@ -1,15 +1,11 @@
-package com.ericmschmidt.classicsreader.datamodel;
+package com.ericmschmidt.classicsreader.datamodel
 
-import static com.ericmschmidt.classicsreader.ApplicationLoggingKt.logError;
-
-import com.ericmschmidt.classicsreader.MyApplication;
-import com.ericmschmidt.classicsreader.utilities.DictionaryXMLHelper;
-import com.ericmschmidt.classicsreader.utilities.ITextConverter;
-import com.ericmschmidt.classicsreader.utilities.ResourceHelper;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Random;
+import com.ericmschmidt.classicsreader.MyApplication.Factory.applicationInstance
+import com.ericmschmidt.classicsreader.logError
+import com.ericmschmidt.classicsreader.utilities.DictionaryXMLHelper
+import com.ericmschmidt.classicsreader.utilities.ITextConverter
+import com.ericmschmidt.classicsreader.utilities.ResourceHelper
+import java.util.Random
 
 /** Contains the data and methods for getting dictionary entries.
  * @author Eric Schmidt
@@ -17,93 +13,68 @@ import java.util.Random;
  * @version 1.5
  * @since 1.1
  */
-public class Dictionary  {
+class Dictionary(val converter: ITextConverter? = null) {
 
-    private ArrayList<String>  _entryHeaders;
-    private final WorkInfo dictionaryInfo;
-    private ITextConverter _converter;
+    val manifest: Manifest = applicationInstance()
+        .manifest
+    val dictionaryInfo: WorkInfo = manifest.dictionaryInfo
+    var entryHeaders: ArrayList<String> = ArrayList()
 
-    private final Manifest manifest;
-
-    /**
-     * Creates a new instance of the dictionary class.
-     */
-    public Dictionary() {
-        this.manifest = MyApplication.Factory
-                .applicationInstance()
-                .getManifest();
-        this.dictionaryInfo = manifest.getDictionaryInfo();
-        initEntries();
-    }
-
-    /**
-     * Creates a new instance of the Dictionary class
-     * and instantiates a text converter for non-Latin orthographies.
-     * @param converter the ITextConverter to use
-     */
-    public Dictionary(ITextConverter converter) {
-        this();
-        this._converter = converter;
+    init {
+       initEntries()
     }
 
     /**
      * Gets the number of entries in this dictionary.
-     * @return integer
      */
-    public int getEntryCount() {
-        return this._entryHeaders.size();
+    fun entryCount(): Int {
+        return this.entryHeaders.size
     }
 
     /**
      * Checks whether a specific string is an entry in the dictionary.
-     * @param searchEntry the entry to check for.
-     * @return boolean
      */
-    public boolean isInDictionary(String searchEntry) {
-        return this._entryHeaders.contains(searchEntry);
+    fun isInDictionary(searchEntry: String?): Boolean {
+        return this.entryHeaders.contains(searchEntry)
     }
 
     /**
      * Gets a definition from the dictionary.
-     * @param searchEntry the entry to search for.
-     * @return String
      */
-    public String getEntry(String searchEntry) {
-        String definition = null;
+    fun getEntry(searchEntry: String?): String? {
+        var definition: String? = null
         try {
             if (isInDictionary(searchEntry)) {
-                InputStream stream = ResourceHelper.getResourceStream(this.dictionaryInfo.getLocation());
-                definition = DictionaryXMLHelper.getEntry(stream, searchEntry, _converter);
+                val stream = ResourceHelper.getResourceStream(this.dictionaryInfo.getLocation())
+                definition = DictionaryXMLHelper.getEntry(stream, searchEntry, converter)
             }
-        } catch (Exception ex) {
-            String errorMessage = ex.getMessage();
-            logError(errorMessage);
+        } catch (ex: Exception) {
+            val errorMessage = ex.message
+            logError(errorMessage)
         }
-        return definition;
+        return definition
     }
 
     /**
-     * Gets a randomly selected from the dictionary
-     * @return String randomly selected dictionary entry.
+     * Gets a randomly selected from the dictionary.
      */
-    public String getRandomEntry() {
-        Random random = new Random();
-        int numberOfEntries = this.getEntryCount();
+    fun getRandomEntry(): String? {
+        val random = Random()
+        val numberOfEntries: Int = entryCount()
 
-        int randomNumber = random.nextInt(numberOfEntries + 1);
-        String randomEntryKey = this._entryHeaders.get(randomNumber);
-        return getEntry(randomEntryKey);
+        val randomNumber = random.nextInt(numberOfEntries + 1)
+        val randomEntryKey: String = this.entryHeaders[randomNumber]
+        return getEntry(randomEntryKey)
     }
 
     // Gets the number of alphabet chapters in dictionary.
-    private void initEntries() {
+    private fun initEntries() {
         try {
-            InputStream stream = ResourceHelper.getResourceStream(manifest.getDictionaryEntryResource());
-            this._entryHeaders = DictionaryXMLHelper.getEntryHeaders(stream);
-
-        } catch (Exception ex) {
-            String errorMessage = ex.getMessage();
-            logError(errorMessage);
+            val stream = ResourceHelper.getResourceStream(manifest.dictionaryEntryResource)
+            this.entryHeaders = DictionaryXMLHelper.getEntryHeaders(stream)
+        } catch (ex: java.lang.Exception) {
+            val errorMessage = ex.message
+            logError(errorMessage)
         }
     }
 }
