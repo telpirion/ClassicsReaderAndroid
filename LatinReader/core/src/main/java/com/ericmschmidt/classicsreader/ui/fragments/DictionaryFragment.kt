a38d2c3fd0447454da56e9d7d1acc45714891958
Eric Schmidt <em.schmidt78@gmail.com>
2025-11-23 15:50:43 -0800

    feat: converts DictionaryFragment to Kotlin

--- LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/ui/fragments/DictionaryFragment.kt
+++ LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/ui/fragments/DictionaryFragment.kt
@@ -1,151 +1,129 @@
-package com.ericmschmidt.classicsreader.ui.fragments;
+package com.ericmschmidt.classicsreader.ui.fragments
 
-import android.content.res.Resources;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import androidx.fragment.app.Fragment;
+import android.os.Bundle
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.ericmschmidt.classicsreader.MyApplication
+import com.ericmschmidt.classicsreader.R
+import com.ericmschmidt.classicsreader.datamodel.Dictionary
+import com.ericmschmidt.classicsreader.utilities.ITextConverter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.Objects
 
-import com.ericmschmidt.classicsreader.MyApplication;
-import com.ericmschmidt.classicsreader.R;
-import com.ericmschmidt.classicsreader.datamodel.Dictionary;
-import com.ericmschmidt.classicsreader.utilities.ITextConverter;
-
-import java.util.Objects;
-
-/** Displays the dictionary page.
+/**
+ * Displays the dictionary page.
  *
  * @author Eric Schmidt
  * @author <a href="https://telpirion.com">...</a>
  * @version 2.0
  * @since 1.0
  */
-public class DictionaryFragment extends Fragment {
-    private String query;
-    private ITextConverter converter;
+class DictionaryFragment : Fragment() {
+    private var query: String? = null
+    private var converter: ITextConverter? = null
+    private var isNonRomanChar = false
 
-    private boolean isNonRomanChar;
-
-    /**
-     * Required empty public constructor
-     */
-    public DictionaryFragment() { }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         // Use safeArgs to get data out of navigation.
-        assert getArguments() != null;
-        DictionaryFragmentArgs args = DictionaryFragmentArgs.fromBundle(getArguments());
-        this.query = args.getDictionaryQuery();
+        arguments?.let {
+            val args = DictionaryFragmentArgs.fromBundle(it)
+            query = args.dictionaryQuery
+        }
 
-        MyApplication.ApplicationInstance applicationInstance = MyApplication.Factory.applicationInstance();
-        this.isNonRomanChar = applicationInstance.isNonRomanChar();
-        if (this.isNonRomanChar) {
-            converter = applicationInstance.getTextConverter();
+        val applicationInstance = MyApplication.Factory.applicationInstance()
+        isNonRomanChar = applicationInstance.isNonRomanChar
+        if (isNonRomanChar) {
+            converter = applicationInstance.textConverter
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dictionary, container, false);
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dictionary, container, false)
     }
 
-    /**
-     * Loads the fragment and the associated ReadingViewModel.
-     * @param onSavedInstanceState Bundle
-     */
-    public void onActivityCreated(Bundle onSavedInstanceState) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        super.onActivityCreated(onSavedInstanceState);
+        val dictionaryProgress = view.findViewById<ProgressBar>(R.id.dictionary_progress)
+        val searchQuery = view.findViewById<EditText>(R.id.search_query)
 
-        // Hide progress bar.
-        getActivity().findViewById(R.id.dictionary_progress).setVisibility(View.INVISIBLE);
-
-        EditText searchQuery = (EditText)getActivity().findViewById(R.id.search_query);
+        dictionaryProgress.visibility = View.INVISIBLE
 
         // Convert text as user types.
-        if (this.isNonRomanChar) {
-
-            TextWatcher watcher = converter != null ?
-                    converter.getTextWatcher(searchQuery) :
-                    Objects.requireNonNull(MyApplication.Factory
-                                    .applicationInstance()
-                                    .getTextConverter())
-                            .getTextWatcher(searchQuery);
-            searchQuery.addTextChangedListener(watcher);
+        if (isNonRomanChar) {
+            val watcher: TextWatcher? = converter?.getTextWatcher(searchQuery)
+                ?: Objects.requireNonNull(
+                    MyApplication.Factory
+                        .applicationInstance()
+                        .textConverter
+                )
+                    ?.getTextWatcher(searchQuery)
+            searchQuery.addTextChangedListener(watcher)
         }
 
-        searchQuery.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        searchQuery.setOnEditorActionListener { v, _, _ ->
+            val searchQueryText = v.text.toString()
+            submitSearchQuery(searchQueryText)
+            false
+        }
 
-                String searchQueryText = v.getText().toString();
-                submitSearchQuery(searchQueryText);
-                return false;
-            }
-        });
-
-        if (query != null) {
-            searchQuery.setText(query);
-            submitSearchQuery(query);
+        query?.let {
+            searchQuery.setText(it)
+            submitSearchQuery(it)
         }
     }
 
     // Sends and receives a search query from the integrated dictionary.
-    private void submitSearchQuery(String query) {
-
-        // Show the indefinite progress indicator.
-        ProgressBar progress = (ProgressBar)getActivity().findViewById(R.id.dictionary_progress);
-        progress.setVisibility(View.VISIBLE);
+    private fun submitSearchQuery(query: String) {
+        val dictionaryProgress = view?.findViewById<ProgressBar>(R.id.dictionary_progress)
+        dictionaryProgress?.visibility = View.VISIBLE
 
         // To improve fragment UI responsiveness, submit query
-        // and load dictionary in an AsyncTask.
-        new SearchDictionaryTask().execute(query);
+        // and load dictionary in a coroutine.
+        lifecycleScope.launch {
+            searchDictionary(query)
+        }
     }
 
-    // Create AsyncTask for fetching and displaying dictionary search result.
-    private class SearchDictionaryTask extends AsyncTask<String, Integer, Long> {
-
-        protected String queryResults = "";
-        private Dictionary dictionary;
-
-        protected Long doInBackground(String... query) {
-
-            String transcribedQuery = query[0];
-
-            if (isNonRomanChar) {
-                transcribedQuery = converter.convertTargetToSourceCharacters(transcribedQuery);
-                dictionary = new Dictionary(converter);
+    private suspend fun searchDictionary(query: String) {
+        val queryResults = withContext(Dispatchers.IO) {
+            val transcribedQuery = if (isNonRomanChar) {
+                converter?.convertTargetToSourceCharacters(query) ?: query
             } else {
-                dictionary = new Dictionary();
+                query
             }
 
-            if(dictionary.isInDictionary(transcribedQuery)) {
-                queryResults = dictionary.getEntry(transcribedQuery);
+            val dictionary = if (isNonRomanChar) {
+                Dictionary(converter)
             } else {
-                Resources resources = getResources();
-                queryResults = resources.getString(R.string.dictionary_query_no_results);
+                Dictionary()
             }
 
-            return  new Long(1);
+            if (dictionary.isInDictionary(transcribedQuery)) {
+                dictionary.getEntry(transcribedQuery)
+            } else {
+                resources.getString(R.string.dictionary_query_no_results)
+            }
         }
 
-        protected void onPostExecute(Long result){
-            TextView resultsField = (TextView)getActivity().findViewById(R.id.dictionary_result);
-            resultsField.setText(queryResults);
+        val resultsField = view?.findViewById<TextView>(R.id.dictionary_result)
+        resultsField?.text = queryResults
 
-            ProgressBar progress = (ProgressBar)getActivity().findViewById(R.id.dictionary_progress);
-            progress.setVisibility(View.INVISIBLE);
-        }
+        val dictionaryProgress = view?.findViewById<ProgressBar>(R.id.dictionary_progress)
+        dictionaryProgress?.visibility = View.INVISIBLE
     }
 }
