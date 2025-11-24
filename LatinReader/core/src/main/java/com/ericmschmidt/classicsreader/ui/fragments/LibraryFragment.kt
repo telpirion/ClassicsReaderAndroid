72e99219cc761100a3d2c1a8b64babb3202a9161
Eric Schmidt <em.schmidt78@gmail.com>
2025-11-23 16:04:52 -0800

    feat: converts LibraryFragment to Kotlin

--- LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/ui/fragments/LibraryFragment.kt
+++ LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/ui/fragments/LibraryFragment.kt
@@ -1,95 +1,96 @@
-package com.ericmschmidt.classicsreader.ui.fragments;
+package com.ericmschmidt.classicsreader.ui.fragments
 
-import static com.ericmschmidt.classicsreader.ApplicationLoggingKt.logError;
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.ericmschmidt.classicsreader.MyApplication
+import com.ericmschmidt.classicsreader.R
+import com.ericmschmidt.classicsreader.activities.MainActivity
+import com.ericmschmidt.classicsreader.datamodel.Library
+import com.ericmschmidt.classicsreader.datamodel.Manifest
+import com.ericmschmidt.classicsreader.datamodel.WorkInfo
+import com.ericmschmidt.classicsreader.logError
+import com.ericmschmidt.classicsreader.ui.interop.setContentToLazyGrid
+import com.ericmschmidt.classicsreader.ui.interop.setContentToLazyList
 
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.compose.ui.platform.ComposeView;
-import androidx.fragment.app.Fragment;
 
-import com.ericmschmidt.classicsreader.MyApplication;
-import com.ericmschmidt.classicsreader.R;
-import com.ericmschmidt.classicsreader.activities.MainActivity;
-import com.ericmschmidt.classicsreader.data.Library;
-import com.ericmschmidt.classicsreader.data.Manifest;
-import com.ericmschmidt.classicsreader.ui.interop.ComposeViewAdapter;
-
-/** Displays works (books) in a RecyclerView.
+/**
+ * Displays works (books) in a RecyclerView.
  *
- *  This class is used for presenting both foreign language and English texts.
+ * This class is used for presenting both foreign language and English texts.
  *
- *  Layout files:
- *  - res/layout/fragment_library.xml
+ * Layout files:
+ * - res/layout/fragment_library.xml
  *
  * @author Eric Schmidt
  * @author <a href="https://telpirion.com">...</a>
- * @version 1.5
+ * @version 2.0
  * @since 1.0
  */
-public class LibraryFragment extends Fragment {
+class LibraryFragment : Fragment() {
 
-    private static final String TAG = "LibraryFragment";
+    private var isTranslation = false
 
-    private boolean isTranslation;
-
-    /**
-     * Required empty public constructor
-     */
-    public LibraryFragment() { }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         // Use safeArgs.
-        assert getArguments() != null;
-        LibraryFragmentArgs args = LibraryFragmentArgs.fromBundle(getArguments());
-        this.isTranslation = args.getIsTranslations();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_library, container, false);
-    }
-
-    public void onActivityCreated(Bundle onSavedInstanceState) {
-        super.onActivityCreated(onSavedInstanceState);
-
-        try {
-            // Retrieve the manifest from the package using config settings.
-            Manifest manifest = MyApplication.Factory
-                    .applicationInstance().getManifest();
-
-            Library library = new Library(manifest.getCollection());
-            Log.i(TAG, "library length = " + library.getWorks().length);
-
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            String displayType = sharedPreferences.getString(SettingsFragment.DISPLAY_TYPE,
-                    SettingsFragment.DISPLAY_TYPE_DEFAULT);
-
-            Log.i(TAG, "displayType = " + displayType);
-
-            ComposeView composeView = (ComposeView) findViewById(R.id.compose_view);
-            if (displayType.equals("Grid")) {
-                ComposeViewAdapter.setContentToLazyGrid(composeView, library,
-                        this.isTranslation, (MainActivity) this.getActivity());
-            } else {
-                ComposeViewAdapter.setContentToLazyList(composeView, library,
-                        this.isTranslation, (MainActivity) this.getActivity());
-            }
-
-        } catch (Exception ex) {
-            logError(this.getClass(), ex.getMessage());
+        arguments?.let {
+            val args = LibraryFragmentArgs.fromBundle(it)
+            isTranslation = args.isTranslations
         }
     }
 
-    private View findViewById(int id) {
-        return this.getView().findViewById(id);
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        try {
+            // Retrieve the manifest from the package using config settings.
+            val manifest: Manifest = MyApplication.Factory
+                .applicationInstance().manifest
+
+            val library = Library(manifest.getCollection() as ArrayList<WorkInfo>)
+            Log.i(TAG, "library length = " + library.collection.size)
+
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity)
+            val displayType = sharedPreferences.getString(
+                SettingsFragment.DISPLAY_TYPE,
+                SettingsFragment.DISPLAY_TYPE_DEFAULT
+            )
+
+            Log.i(TAG, "displayType = $displayType")
+
+            val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+            if (displayType == "Grid") {
+                setContentToLazyGrid(
+                    composeView, library,
+                    isTranslation, activity as MainActivity
+                )
+            } else {
+                setContentToLazyList(
+                    composeView, library,
+                    isTranslation, activity as MainActivity
+                )
+            }
+
+        } catch (ex: Exception) {
+            logError(this.javaClass, ex.message)
+        }
+    }
+
+    companion object {
+        private const val TAG = "LibraryFragment"
     }
 }
