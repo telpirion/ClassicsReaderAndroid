7e389420d69e806fba5b583da05a487fa81910b7
Eric Schmidt <erschmid@google.com>
2025-12-02 14:53:28 -0800

    fix: removes cyclical companion object calls in MyApplication

--- LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/MyApplication.kt
+++ LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/MyApplication.kt
@@ -24,52 +24,61 @@ class MyApplication : Application() {
         val textConverter: ITextConverter?,
     )
 
+    var manifest: Manifest? = null
+        private set
+    var isNonRomanChar: Boolean? = null
+        private set
+    var converter: ITextConverter? = null
+        private set
+
+    private fun getTextConverter(isNonRomanChar: Boolean): ITextConverter? {
+        var converter: ITextConverter? = null
+        if (isNonRomanChar) {
+            val className = instance.resources.getString(R.string.text_converter)
+            try {
+                val manifestClass = Class.forName(className)
+                val constructors = manifestClass.constructors
+                converter = constructors[0].newInstance() as ITextConverter
+            } catch (ex: Exception) {
+                Log.e(Manifest::class.java.name, ex.message as String)
+            }
+        }
+        return converter
+    }
+
     init {
         instance = this
     }
+
+    fun populateFields() {
+        Log.i("MyApplication", "populating Application fields")
+        if (manifest == null) {
+            val manifestName =
+                instance.resources.getString(R.string.manifest)
+            manifest = Manifest.getManifest(manifestName)!!
+        }
+        if (isNonRomanChar == null) {
+            isNonRomanChar = instance.resources.getBoolean(R.bool.non_roman_char)
+        }
+        if (converter == null) {
+            if (getTextConverter(isNonRomanChar as Boolean) != null) {
+                converter = getTextConverter(isNonRomanChar as Boolean)
+            }
+        }
+    }
+
 
     companion object Factory {
         lateinit var instance: MyApplication
             private set
 
-        /**
-         * Get the manifest of texts for this app.
-         * @return Manifest
-         */
-        private fun getManifest(): Manifest? {
-            val manifestName =
-                instance.resources.getString(R.string.manifest)
-            Log.i("MyApplication", "manifestName = $manifestName")
-            return Manifest.getManifest(manifestName)
-        }
-
-        private fun isNonRomanChar(): Boolean {
-            return instance.resources.getBoolean(R.bool.non_roman_char)
-        }
-
-        private fun getTextConverter(): ITextConverter? {
-            var converter: ITextConverter? = null
-
-            if (isNonRomanChar()) {
-                val className = instance.resources.getString(R.string.text_converter)
-
-                try {
-                    val manifestClass = Class.forName(className)
-                    val constructors = manifestClass.constructors
-                    converter = constructors[0].newInstance() as ITextConverter
-                } catch (ex: Exception) {
-                    Log.e(Manifest::class.java.name, ex.message as String)
-                }
-            }
-            return converter
-        }
-
         fun applicationInstance(): ApplicationInstance {
+            instance.populateFields()
             return ApplicationInstance(
                 context = instance,
-                manifest = getManifest() as Manifest,
-                isNonRomanChar = isNonRomanChar(),
-                textConverter = getTextConverter()
+                manifest = instance.manifest as Manifest,
+                isNonRomanChar = instance.isNonRomanChar as Boolean,
+                textConverter = instance.converter
             )
         }
     }
