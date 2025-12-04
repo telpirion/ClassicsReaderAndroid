80e3a0f8955d1d4b4b8bafcb6ec0134a823cfe8e
Eric Schmidt <erschmid@google.com>
2025-12-04 15:08:32 -0800

    feat: changes color scheme for details pane.
    # Conflicts:
    #	LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/data/WorkInfo.java

--- LatinReader/compose/src/main/java/com/telpirion/compose/ui/components/DetailsPane.kt
+++ LatinReader/compose/src/main/java/com/telpirion/compose/ui/components/DetailsPane.kt
@@ -11,13 +11,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -25,6 +29,8 @@ import androidx.compose.ui.unit.dp
 import com.ericmschmidt.classicsreader.data.Library
 import com.ericmschmidt.classicsreader.data.WorkInfo
 import com.ericmschmidt.classicsreader.ui.components.LibraryPreviewProvider
+import com.telpirion.compose.ui.theme.Purple40
+import com.telpirion.compose.ui.theme.PurpleGrey80
 
 
 @Preview(showBackground = true)
@@ -48,7 +54,13 @@ fun DetailsPane(
     Card(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        colors = CardColors(
+            containerColor = PurpleGrey80,
+            contentColor = lightColorScheme().onSurface,
+            disabledContainerColor = lightColorScheme().onSurface.copy(alpha = 0.12f),
+            disabledContentColor = lightColorScheme().onSurface.copy(alpha = 0.38f)
+        )
     ) {
         if (workInfo != null) {
             Column(
@@ -91,7 +103,11 @@ fun DetailsPane(
                 Button(
                     onClick = { onReadClick(workInfo.id) },
                     modifier = Modifier
-                        .width(120.dp)
+                        .width(120.dp),
+                    colors = buttonColors(
+                        containerColor = Purple40,
+                        contentColor = Color.White
+                    )
                 ) {
                     Text("Read")
                 }
---
+++ LatinReader/core/src/main/java/com/ericmschmidt/classicsreader/data/WorkInfoExtensions.kt
@@ -0,0 +1,9 @@
+package com.ericmschmidt.classicsreader.data
+
+var WorkInfo.translator: String
+    get() {
+       return this.translator
+    }
+    set(value) {
+        this.translator = value
+    }
