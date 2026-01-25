a41b0b23825a2fa2e155d9a9a8e1bd4c33bc448a
Eric Schmidt <em.schmidt78@gmail.com>
2026-01-24 16:10:32 -0800

    feat: adding new theme files to LatinReader

--- LatinReader/latinreader/src/main/java/com/ericmschmidt/latin/LatinReaderApplication.kt
+++ LatinReader/latinreader/src/main/java/com/ericmschmidt/latin/LatinReaderApplication.kt
@@ -2,5 +2,7 @@ package com.ericmschmidt.latin
 
 import com.ericmschmidt.classicsreader.MyApplication
 
-class LatinReaderApplication: MyApplication(libraryName = "com.ericmschmidt.latin.data.LatinReaderLibrary") {
+class LatinReaderApplication: MyApplication(
+    libraryName = "com.ericmschmidt.latin.data.LatinReaderLibrary",
+) {
 }
