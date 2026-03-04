6321aefd17f7d7a117ea561e6c42f421b05dec0d
Eric Schmidt <erschmid@google.com>
2026-03-03 16:06:27 -0800

    fix: changed Close button weird background

--- LatinReader/compose/src/main/java/com/telpirion/compose/ui/components/SupportingPane.kt
+++ LatinReader/compose/src/main/java/com/telpirion/compose/ui/components/SupportingPane.kt
@@ -2,7 +2,6 @@
 
 package com.telpirion.compose.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -178,7 +177,6 @@ fun SupportingPaneTemplate(
             Icon(
                 Icons.Default.Close,
                 contentDescription = "Close",
-                modifier = Modifier.background(MaterialTheme.colorScheme.secondary)
             )
         }
     }
